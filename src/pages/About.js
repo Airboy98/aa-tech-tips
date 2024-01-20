fbd66dfdc207ed3adee689f97b569362e5f2235d
Airboy98 <Airboy98@users.noreply.github.com>
2024-01-19 19:10:14 -0600

    added footer and other minor edits to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,15 +6,23 @@ export default function About() {
       <h1>About</h1>
       <hr></hr>
       This site is, first and foremost, an opportunity the Lord has given me to
-      assist and serve others with technology. I wanted to create this website
-      to help out those who are less technologically inclined to offer helpful
-      tips and streamline resolving common issues they may have with modern
-      tech. Along with the tips, helpful links to brand websites are provided.
+      assist and serve others with technology. I wanted to use my experience
+      with IT support and CS degree to create this website to help out those who
+      are less technologically inclined. This site offers helpful tips and
+      streamlines resolving common issues with modern tech. Along with the tips,
+      helpful links to brand websites are provided.
       <br></br>
       <br></br>
       <img src="images/moon2.jpg" height="250" width="200" />
       {/* <br></br> */}
-      <h4>Aaron Turner - Webmaster</h4>
+      <footer>
+        <p>Developed and Designed by Aaron Turner</p>
+        <p>AA Tech Tips Est. 2024</p>
+        <p>
+          <a href="mailto:feedback@aatechtips.com">feedback@aatechtips.com</a>
+        </p>
+        <p>Copyright © 2024 - All Rights Reserved</p>
+      </footer>
     </>
   );
 }
